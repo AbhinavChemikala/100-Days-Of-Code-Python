4bac81f5ed56ff61b2b905758ac3c466675e9d72
Abhinav Chemikala <abhigamez1@gmail.com>
2022-02-09 21:25:03 +0530

    first commit

---
+++ 100 Days of code/Day1.py
@@ -0,0 +1,33 @@
+# Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Using \n to print on a new line
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+# String concatenation
+print("Hello" + " " + "Abhinav")
+
+# input will get user input to console
+print("Hello " + input("What is your name? "))
+
+# prints the number of characters in a user's name
+# len() function is used to find the length of a string
+print(len(input("Enter your name: ")))
+
+
+#Variables
+name = input("Enter your name: ")
+length = len(name)
+print(length)
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+#2. Ask the user for the city that they grew up in.
+city = input("What city were you born in? \n")
+#3. Ask the user for the name of a pet.
+pet = input("What is your pet's name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city +" " + pet)
+
