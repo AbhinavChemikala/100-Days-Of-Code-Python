4d6d7d96f7d7ccfe3f74223383973fb2ca00e599
Abhinav Chemikala <abhigamez1@gmail.com>
2022-02-14 00:10:12 +0530

    Day6

---
+++ 100 Days of code/Day6.py
@@ -0,0 +1,19 @@
+# Defining Functions
+def my_function():
+    print("Hello")
+    print("Bye!")
+
+my_function() # Calling the function
+
+# While Loops
+number = 5
+while number > 0: # While loop
+    print(number) # Prints 5, 4, 3, 2, 1
+    number -= 1 # number = number - 1
+
+number = 3
+# While not loop
+while not number == 0: 
+    print(number) # Prints 5, 4, 3, 2, 1
+    number -= 1 # number = number - 1
+
