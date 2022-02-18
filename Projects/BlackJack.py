0e489052fe9c227eda6096a75ff122a2dafab055
Abhinav Chemikala <abhigamez1@gmail.com>
2022-02-19 00:10:05 +0530

    Day12

--- Projects/BlackJack.py
+++ Projects/BlackJack.py
@@ -1,3 +1,5 @@
+import random
+from os import system
 logo = """
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -8,72 +10,75 @@ logo = """
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
-print(logo)
-import random
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def deal_card(random_card):
-    return random.choice(random_card)
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-user_cards = []
-computer_cards = []
-user_cards.append(deal_card(cards))
-computer_cards.append(deal_card(cards))
-
-def calculate_score(card_list):
-    if len(card_list) == 2 and sum(card_list) == 21:
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+    if sum(cards) == 21 and len(cards) == 2:
         return 0
-    if 11 in card_list and sum(card_list) > 21:
-        card_list.remove(11)
-        card_list.append(1)
-    return sum(card_list)
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
-def blackjack():
-    should_continue = True
-    while should_continue:
-        player_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
-        print(f"Your cards: {user_cards}, current score: {player_score}.")
-        print(f"Dealer first cards: [{computer_cards[0]}], current score: {computer_score}.")
-        if player_score == 0 or computer_score == 0 or player_score >21:
-            print("Game end")
-            should_continue = False
-        else:
-            if input("Do you want to draw another card? 'y' or 'n': ") == 'y':
-                user_cards.append(deal_card(cards))
-            else:
-                print(f"Your final cards: {user_cards}, current score: {player_score}.")
-                should_continue = False
-                while computer_score < 17:
-                    computer_cards.append(deal_card(cards))
-                    computer_score = calculate_score(computer_cards)
-                print(f"Dealer final cards: {computer_cards}, current score: {computer_score}.")
-                
-    def compare(plyr_score, comp_score):
-        if plyr_score == comp_score:
-            print("Its a Draw.")
-        elif comp_score == 0:
-            print("Player lost. Dealer got a black jack")
-        elif plyr_score == 0:
-            print("Player won. Player got a black jack")
-        elif plyr_score > 21:
-            print("Player lose. Player score is more than 21")
-        elif comp_score > 21:
-            print("Computer lose. Computer score is more than 21")
-        else:
-            if plyr_score > comp_score:
-                print(f"Player won. Player has {plyr_score} and Dealer has {comp_score}")
-            else:
-                print(f"Computer won. Player has {plyr_score} and Dealer has {comp_score}")
+def compare(user_score, computer_score):
+    #Bug fix. If you and the computer are both over, you lose.
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose 😤"
     
-    compare(player_score, computer_score)
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
 
-blackjack()
-
-if input("Do you want to restart the game? 'y' or 'n': ") == 'y':
-    from os import system
-    system("clear") # Clearing the screen
+def play_game():
     print(logo)
-    blackjack()
-else:
-    print("good bye.")
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"   Your cards: {user_cards}, current score: {user_score}")
+        print(f"   Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"   Your final hand: {user_cards}, final score: {user_score}")
+    print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    system("clear") # Clearing the screen
+    play_game()
---
+++ Projects/Guess_The_Number.py
@@ -0,0 +1,54 @@
+import random
+logo = """
+   _____                       _______ _            _   _                 _               
+  / ____|                     |__   __| |          | \ | |               | |              
+ | |  __ _   _  ___  ___ ___     | |  | |__   ___  |  \| |_   _ _ __ ___ | |__   ___ _ __ 
+ | | |_ | | | |/ _ \/ __/ __|    | |  | '_ \ / _ \ | . ` | | | | '_ ` _ \| '_ \ / _ \ '__|
+ | |__| | |_| |  __/\__ \__ \    | |  | | | |  __/ | |\  | |_| | | | | | | |_) |  __/ |   
+  \_____|\__,_|\___||___/___/    |_|  |_| |_|\___| |_| \_|\__,_|_| |_| |_|_.__/ \___|_|   
+                                                                                          
+                                                                                          
+"""
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+def guess_number():
+    """Returns a random number between 1 and 100."""
+    guess = random.randint(1,100)
+    return guess
+
+def choose_difficulty():
+    """Returns a difficulty level."""
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        lives = 10
+    else:
+        lives = 5
+        
+    return lives
+
+main_number = guess_number()
+user_lives = choose_difficulty()
+print(f"pstt the answer is {main_number}")
+
+should_continue = True
+
+while should_continue and user_lives > 0:
+    print(f"You have {user_lives} attempts remaining to guess the number.")
+    user_guess = int(input("Make a guess: "))
+    if user_guess == main_number:
+        print(f"You got it! The answer was {main_number}.")
+        should_continue = False
+    elif user_guess < main_number:
+        user_lives -= 1
+        print("Too low.")
+        print("Guess again.")
+    else:
+        user_lives -= 1
+        print("Too high.")
+        print("Guess again.")
+
+if user_lives == 0:
+    print("You've run out of guesses, you lose.")
